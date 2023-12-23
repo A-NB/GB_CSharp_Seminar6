2a8464d7647b34e80fe54ef39474c2607b2bc9dc
A-NB <bel-an510@yandex.ru>
2023-12-23 14:17:12 +0300

    added all tasks to repo (1 ... 4)

---
+++ Task1/Program.cs
@@ -0,0 +1,85 @@
+﻿// Знакомство с языками программирования (семинары)
+// Урок 6. Массивы и строки
+
+// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+char[,] GetRandom2DArrayOfChar()
+{
+    string st = "0123456789abcdefjhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯЁ~@#$%^&!№;%:?*()_+/-.,\"<>";
+    Random rnd = new Random();
+    int row = rnd.Next(2, 11);
+    int col = rnd.Next(2, 11);
+    char[,] array = new char[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = st[rnd.Next(rnd.Next(0, st.Length))];
+        }
+    }
+    return array;
+}
+
+void PrintArray(char[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+string CreateString(char[,] array)
+{
+    string st = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            st += array[i, j];
+        }
+    }
+    return st;
+}
+
+char[,] array = GetRandom2DArrayOfChar();
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine(CreateString(array));
+
+
+
+// **************************** "ИДЕАЛЬНОЕ РЕШЕНИЕ" *****************************///
+// 6 - ой семинар:
+// Задача 1: Создание строки из двумерного массива символов
+/* public class Task1
+{
+    public static void Main(string[] args)
+    {
+        // Инициализация двумерного массива символов
+        char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+        // Вызов метода для создания строки из 2D массива
+        string result = CreateStringFrom2DArray(charArray);
+        // Вывод результата
+        Console.WriteLine(result);
+    }
+    // Метод для создания строки из двумерного массива символов
+    public static string CreateStringFrom2DArray(char[,] array)
+    {
+        string result = "";
+        // Цикл по каждому элементу в двумерном массиве
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                // Добавление каждого символа в результирующую строку
+                result += array[i, j];
+            }
+        }
+        return result;
+    }
+} */
---
+++ Task4/Program.cs
@@ -0,0 +1,44 @@
+﻿// Знакомство с языками программирования (семинары)
+// Урок 6. Массивы и строки
+
+// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке. В полученной строке слова должны быть также разделены пробелами.
+
+
+string st = "I have two eyes and I can see a book and pen infront of me"; //Hello my world";
+
+string newSt = string.Empty, sep = " ";
+string[] stArray = st.Split(" ");
+
+for (int i = stArray.Length - 1; i >= 0; i--)
+{
+    if (i == 0) sep = string.Empty;
+    newSt += (stArray[i] + sep);
+}
+
+System.Console.WriteLine($"\"{newSt}\"");
+
+
+// ************************************ " ИДЕАЛЬНОЕ РЕШЕНИЕ"  ****************************//
+// Задача 4 (необязательная): Обратный порядок слов в строке
+/* public class Task4
+{
+    public static void Main(string[] args)
+    {
+        // Входная строка со словами, разделенными пробелами
+        string input = "Hello my world";
+        // Вызов метода для обращения порядка слов в строке
+        string result = ReverseWords(input);
+        // Вывод результата
+        Console.WriteLine(result);
+    }
+    // Метод для обращения порядка слов в строке
+    public static string ReverseWords(string str)
+    {
+        // Разделение строки на слова
+        string[] words = str.Split(' ');
+        // Обращение порядка слов
+        Array.Reverse(words);
+        // Соединение слов обратно в строку с пробелами
+        return string.Join(" ", words);
+    }
+} */
